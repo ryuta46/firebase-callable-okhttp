f243a084a2fe47c7b8b9580a102fa09837d90001
ryuta46 <ryuta46g@gmail.com>
2019-01-16 08:22:47 +0900

    Add callbale function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,5 +4,17 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
+});
+
+
+export const helloWorldOnCall = functions.https.onCall((data, context) => {
+    const text = data.text;
+    const uid = context.auth.uid;
+
+    return {
+        message: "Hello World from onCall.",
+        uid: uid,
+        text: text
+    };
 });
